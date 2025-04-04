ff9f96eebf02dfa8b99ebd7d0d6e043119ace145
Albert <slowraq@gmail.com>
2025-04-04 23:20:24 +0300

    fixes

--- src/entities/City/ui/CityDetails/model/selectors/getCityForecast.ts
+++ src/entities/City/ui/CityDetails/model/selectors/getCityForecast.ts
@@ -2,6 +2,3 @@ import {RootState} from "@/app/providers/StoreProvider/config/store";
 
 export const getCityForecastById = (id: string) => (state: RootState)  =>
     state.cityForecast.data[id];
-
-export const getCityForecastLoading = (state: RootState) =>
-    state.cityForecast.isLoading ?? false;
